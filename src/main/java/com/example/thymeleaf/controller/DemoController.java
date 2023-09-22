ee5a8b98db4a43aff00e7389540a84f1d58f6649
vishesh <visheshtiwari@ggx.io>
2023-09-22 17:18:29 +0530

    added required field in controller

--- src/main/java/com/example/thymeleaf/controller/DemoController.java
+++ src/main/java/com/example/thymeleaf/controller/DemoController.java
@@ -1,5 +1,6 @@
 package com.example.thymeleaf.controller;
 
+import com.example.thymeleaf.model.Customer;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,9 +8,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class DemoController {
 
-    @GetMapping("/hello")
-    public String sayHello(Model theModel) {
-        theModel.addAttribute("theDate", new java.util.Date());
-        return "helloworld";
+    @GetMapping("/")
+    public String showForm(Model theModel) {
+        theModel.addAttribute("theCustomer", new Customer());
+
+        return "customer-from";
     }
 }
