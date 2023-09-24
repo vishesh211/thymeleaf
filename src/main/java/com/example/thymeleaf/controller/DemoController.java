b3bebd0fcf3141bad73be36d46a013347925510c
vishesh <visheshtiwari@ggx.io>
2023-09-24 16:16:49 +0530

    added @initbinder to preprocess any requests to the controller

--- src/main/java/com/example/thymeleaf/controller/DemoController.java
+++ src/main/java/com/example/thymeleaf/controller/DemoController.java
@@ -2,15 +2,24 @@ package com.example.thymeleaf.controller;
 
 import com.example.thymeleaf.model.Customer;
 import jakarta.validation.Valid;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class DemoController {
+
+    @InitBinder
+    public void initBind(WebDataBinder dataBinder) {
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+        dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+    }
 
     @GetMapping("/")
     public String showForm(Model theModel) {
@@ -24,6 +33,8 @@ public class DemoController {
             @Valid @ModelAttribute ("customer") Customer theCustomer,
             BindingResult theBindingResult
     ) {
+        System.out.println("Last Name: |" + theCustomer.getLastName() + "|");
+
         if (theBindingResult.hasErrors()) {
             return "customer-form";
         } else {
