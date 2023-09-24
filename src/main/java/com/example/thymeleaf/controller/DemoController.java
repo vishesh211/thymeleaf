9504cd6463158c3d7894ef128cb43cabf25165db
vishesh <visheshtiwari@ggx.io>
2023-09-24 15:47:22 +0530

    created customer-form.html

--- src/main/java/com/example/thymeleaf/controller/DemoController.java
+++ src/main/java/com/example/thymeleaf/controller/DemoController.java
@@ -1,17 +1,33 @@
 package com.example.thymeleaf.controller;
 
 import com.example.thymeleaf.model.Customer;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class DemoController {
 
     @GetMapping("/")
     public String showForm(Model theModel) {
-        theModel.addAttribute("theCustomer", new Customer());
+        theModel.addAttribute("customer", new Customer());
 
-        return "customer-from";
+        return "customer-form";
+    }
+
+    @PostMapping("/processForm")
+    public String processForm(
+            @Valid @ModelAttribute ("customer") Customer theCustomer,
+            BindingResult theBindingResult
+    ) {
+        if (theBindingResult.hasErrors()) {
+            return "customer-form";
+        } else {
+            return "customer-confirmation";
+        }
     }
 }
